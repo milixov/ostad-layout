6d03df87fceecdc777db4b47c539e093cbb6e862
Milad Moafi <miladmoafi@outlook.com>
2019-05-26 11:42:16 +0430

    Test layout generator basic idea

---
+++ src/component/LayoutGenerator.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+const layoutData = [{
+    title: "XL", structure:
+        [
+            {flex: 1, column: []},
+            {flex: 1, column: null}
+        ]
+}, {
+    title: "2XL", structure:
+        [
+            {flex: 1, column: []},
+            {flex: 1, column: []}
+        ]
+}, {
+    title: "4L", structure:
+        [
+            {flex: 1, column: []},
+            {flex: 1, column: []},
+            {flex: 1, column: []},
+            {flex: 1, column: []}
+        ]
+}, {
+    title: "XL/2L", structure:
+        [
+            {flex: 2, column: []},
+            {flex: 1, column: []},
+            {flex: 1, column: []}
+        ]
+},
+    {
+        title: "XL/L/2SM", structure:
+            [
+                {flex: 2, column: []},
+                {flex: 1, column: []},
+                {
+                    flex: 1, column: [
+                        {flex: 1}, {flex: 1}
+                    ]
+                }
+            ]
+    }
+];
+
+class LayoutGenerator extends Component {
+
+    static randomColor() {
+        return '#'+Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    static style(flex) {
+        return {flex, backgroundColor: LayoutGenerator.randomColor(), justifyContent: 'center', alignItems: 'center'}
+    }
+
+    render() {
+        const {layout} = this.props;
+        let mLayout = layoutData.find(item => item.title === layout);
+
+        return (
+            <View style={{flex: 1, flexDirection: 'column', marginTop: 20}}>
+                {mLayout ? mLayout.structure.map((item, index) =>
+                    <View key={"row_" + index} style={LayoutGenerator.style(item.flex)}>
+                        {item.column !== null ?
+                            item.column.length > 0 ?
+                                <View style={{flex: 1, flexDirection: 'row'}}>
+                                    {item.column.map((col, colIndex) =>
+                                        <View key={"col_" + colIndex} style={LayoutGenerator.style(item.flex)}>
+                                            <Text>Plot</Text>
+                                        </View>)}
+                                </View> :
+                                <Text>Plot</Text> :
+                            null}
+                    </View>
+                ) : null}
+            </View>
+        );
+    }
+}
+
+export default LayoutGenerator;
+
+
